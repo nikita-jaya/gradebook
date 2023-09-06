98ea68d13a5534c7ca34a29ef8675dc488999ee8
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-09-06 05:40:53 -0700

    convert_to_min function

--- R/first_functions.R
+++ R/first_functions.R
@@ -10,3 +10,18 @@
 add <- function(x, y) {
     x + y
 }
+
+#' Convert hms format to minutes using lubridate
+#' 
+#' @param hms A time in hms format
+#' @returns A numeric vector.
+#' @export
+#' @examples
+#' convert_to_min("11:11:11")
+#' convert_to_min("22:00:00")
+convert_to_min <- function(hms){
+    save <- lubridate::hms(hms)
+    save <- lubridate::period_to_seconds(save)
+    save <- save/60
+    return (save)
+}
