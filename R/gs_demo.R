84c2a0f4208151b973e879eaa4cc5ff7a85a441e
andrewpbray <andrewbray@berkeley.edu>
2023-09-12 16:13:01 -0700

    initial commit

---
+++ R/gs_demo.R
@@ -0,0 +1,11 @@
+#' TBD
+#'
+#' @format ## `gs_demo`
+#' TBD
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+"gs_demo"
