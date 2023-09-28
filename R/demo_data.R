1d7410c468260f820403afe192f552dea81e5654
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-09-28 11:54:58 -0700

    demo data documentation

---
+++ R/demo_data.R
@@ -0,0 +1,19 @@
+#' Demo Data
+#'
+#' A fake dataset that mimics Gradescope .csv format
+#' 
+#'
+#' @format ## `demo_data`
+#' A data frame with 10 rows and 44 columns:
+#' \describe{
+#'   \item{Names}{student name}
+#'   \item{Email}{student email}
+#'   \item{SID}{student ID number}
+#'   \item{Sections}{student class section}
+#'   \item{Lab 1}{assignment called Lab 1}
+#'   \item{Lab 1 - Max Points}{maximum possible points for Lab 1}
+#'   \item{Lab 1 - Lateness (H:M:S)}{how late Lab 1 was turned in, formatted HH:MM:SS}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"demo_data"
