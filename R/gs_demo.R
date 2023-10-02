e48c223663f509cda6979b4feca2212ab8e4e1ee
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-10-02 11:22:21 -0700

    gs_demo documentation

--- R/gs_demo.R
+++ R/gs_demo.R
@@ -13,6 +13,8 @@
 #'   \item{Lab 1}{assignment called Lab 1}
 #'   \item{Lab 1 - Max Points}{maximum possible points for Lab 1}
 #'   \item{Lab 1 - Lateness (H:M:S)}{how late Lab 1 was turned in, formatted HH:MM:SS}
+#'   \description{All following assignments follow same three-column format of Lab 1.}
+#'   \description{Additional assignments in gs_demo are as follows: Lab 2, Lab 3, Lab 4, Lab 5, Lab 6, Discussion 1, Discussion 2, Midterm and Final}
 #'   ...
 #' }
 "gs_demo"
