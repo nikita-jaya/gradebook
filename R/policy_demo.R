33b6873008971aa96a2531d915767a8dec306bf5
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-10-02 11:58:20 -0700

    attempting policy_demo documentation

--- R/policy_demo.R
+++ R/policy_demo.R
@@ -6,8 +6,14 @@
 #' @format ## `policy_demo`
 #' A demo policy file
 #' \describe{
-#'   \item{TBD}{student name}
-#'   \description{TBD}
+#'   \description{Coursewide: includes class info}
+#'   \item{course_name}{course name}
+#'   \item{description}{any relevant course description, purely for the user}
+#'   \description{Categories: each category in syllabus}
+#'   \item{name}{category name}
+#'   \item{slipdays}{number of slip days}
 #'   ...
+#'   \description{Cutoff: grade cutoffs}
+#'   \item{Cutoff}{each cutoff reflects the lower bound for each letter grade: A,B,C,D,F}
 #' }
 "policy_demo"
