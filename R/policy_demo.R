7456817b567792a6703039bf461b38e2c288b0e2
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-10-02 11:53:00 -0700

    trial for documenting policy demo

---
+++ R/policy_demo.R
@@ -0,0 +1,13 @@
+#' Policy Demo File
+#'
+#' A fake policy file that mimics a hypothetical class syllabus
+#' 
+#'
+#' @format ## `policy_demo`
+#' A demo policy file
+#' \describe{
+#'   \item{TBD}{student name}
+#'   \description{TBD}
+#'   ...
+#' }
+"policy_demo"
