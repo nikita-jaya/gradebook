7380e7facf26d340832e34a5a2d167219d09ab48
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-10-04 20:58:03 -0700

    tests for check_cat_names

--- tests/testthat/test-validate-policy.R
+++ tests/testthat/test-validate-policy.R
@@ -54,3 +54,30 @@ test_that("check names - one of categories is incorrectly formatted", {
     
     expect_error(check_names(policy))
 })
+
+test_that("check cat names - correct formatting", {
+    policy <- list(coursewide = list(course_name = "Course Name",
+                                     description = "course description"),
+                   categories = list(
+                       list(name = "Name", slipdays = 0, late_time1 = "00:00:00",
+                            late_time2 = "00:00:00", late_scale1 = 0, late_scale2 = 0,
+                            after = TRUE, weight = 0, drops = 0, weighted_method = "equally",
+                            clobber = "None", assigns = c())
+                   ),
+                   cutoff = list(A = 90, B = 80, C = 70, D = 60, F = 0))
+    category_names <- purrr::map(policy$categories, names) |> unlist()
+    expect_true(check_cat_names(category_names))
+})
+
+test_that("check cat names - correct formatting", {
+    policy <- list(coursewide = list(course_name = "Course Name",
+                                     description = "course description"),
+                   categories = list(
+                       list(name = "Name", slipdays = 0, late_time1 = "00:00:00",
+                            late_time2 = "00:00:00",after = TRUE, weight = 0, drops = 0, weighted_method = "equally",
+                            clobber = "None", assigns = c())
+                   ),
+                   cutoff = list(A = 90, B = 80, C = 70, D = 60, F = 0))
+    category_names <- purrr::map(policy$categories, names)  |> unlist()
+    expect_false(check_cat_names(category_names))
+})
