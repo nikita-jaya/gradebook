53dd5820239a29e04a7c0b178bc77835b6dcd291
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-10-30 12:53:44 -0700

    updating calculate_grades2

--- R/calculate-grades2.R
+++ R/calculate-grades2.R
@@ -18,10 +18,10 @@ by_points <- function(gs_data, sub_category, sub_assigns){
     sub_category_max_name <- paste0(sub_category, "_-_max_points") #name of new subcategory max points column
     
     gs_data <- gs_data |>
-        mutate("{sub_category_raw_name}" := rowSums(across(raw_cols)), #compute raw points for new assignment/category
-               "{sub_category_raw_name}" := rowSums(across(max_cols)), #compute max points for new assignment/category
+        mutate("{sub_category_raw_name}" := rowSums(across(all_of(raw_cols))), #compute raw points for new assignment/category
+               "{sub_category_max_name}" := rowSums(across(all_of(max_cols))), #compute max points for new assignment/category
                "{sub_category}_-_percent" := get(sub_category_raw_name)/get(sub_category_max_name) #compute percents for new assignment/category
-               ) 
+        ) 
     return (gs_data)
 }
 
@@ -52,35 +52,63 @@ by_percent <- function(gs_data, sub_category, sub_assigns){
     return (gs_data)
 }
 
-# by_weight <- function(gs_data, aggregate_assign, sub_assigns, weights) {
-#     percent_cols <- paste0(sub_assigns, "_score")
-#     
-#     total_percent <- gs_data |> #get total score for category
-#         rowwise() |>
-#         summarize(raw = sum(c_across(percent_cols)*weights)) %>% #uses weights
-#         pull()
-#     
-#     max_cols <- paste0(sub_assigns, "_-_max_pts")
-#     
-#     total_max <- gs_data |> #get total max_pts for category
-#         rowwise() |>
-#         summarize(raw = sum(c_across(max_cols))) %>%
-#         pull()
-#     
-#     gs_data <- gs_data |>
-#         mutate(raw = total_percent*total_max, #computed raw_pts for category
-#                score = total_percent, #score for category
-#                max = total_max) #total max_pts for category
-#     
-#     l <- length(colnames(gs_data))
-#     #below is naming columns by my convention of `lab1`, `lab1_score` and `lab1_max_pts`
-#     colnames(gs_data)[(l-2):l] <- paste0(aggregate_assign, c("", "_score", "_-_max_pts"))
-#     
-#     return (gs_data)
-#     
-# } #need to re-code
-by_max <- function(gs_data){
-} #need to code
+#' Calculate Grade by Maximum Score in Category
+#' 
+#' This functions calculates the grade of a category by weighting assignments by 
+#' given weights.
+#'
+#' @param gs_data a wide formatted, processed Gradescope data
+#' @param sub_category name of sub-category grade that is being calculated
+#' @param sub_assigns assignments within this subcategory
+#' @param weights weights in same order as `sub_assigns`
+#'
+#' @return a wide formatted dataframe with raw score, max points and percent for new subcategory
+#' @importFrom dplyr mutate across rowwise summarize
+#' @export
+by_weight <- function(gs_data, sub_category, sub_assigns, weights) {
+    percent_cols <- paste0(sub_assigns, "_-_percent") #names of raw score columns
+    
+    total_percent <- gs_data |> #get total score for category
+        rowwise() |>
+        summarize("{sub_category}" := sum(c_across(percent_cols)*weights)/sum(weights))
+    
+    gs_data <-cbind(gs_data, total_percent)
+    
+    return (gs_data)
+    
+}
+
+#' Calculate Grade by Maximum Score in Category
+#' 
+#' This functions calculates the grade of a category with the maximum percentage score of the category.
+#' The max points of this new category is given by the average of the max points of assignments within
+#' this category.
+#'
+#' @param gs_data a wide formatted, processed Gradescope data
+#' @param sub_category name of sub-category grade that is being calculated
+#' @param sub_assigns assignments within this subcategory
+#'
+#' @return a wide formatted dataframe with raw score, max points and percent for new subcategory
+#' @importFrom dplyr mutate across rowwise
+#' @export
+by_max <- function(gs_data, sub_category, sub_assigns){
+    percent_cols <- paste0(sub_assigns, "_-_percent") #names of raw score columns
+    max_cols <- paste0(sub_assigns, "_-_max_points") #names of max points columns
+    
+    sub_category_percent_name <- paste0(sub_category, "_-_percent") #name of new subcategory raw score column
+    sub_category_max_name <- paste0(sub_category, "_-_max_points") #name of new subcategory max points column
+    
+    gs_data <- gs_data |>
+        rowwise() |>
+        mutate("{sub_category_percent_name}" := max(across(all_of(percent_cols))) #compute raw points for new assignment/category
+        )  |> ungroup() |>
+        mutate(
+            "{sub_category_max_name}" := rowMeans(across(all_of(max_cols))), #compute max points for new assignment/category
+            "{sub_category}_-_raw_score" := get(sub_category_percent_name)*get(sub_category_max_name) #compute percents for new assignment/category
+        )
+    
+}
+
 by_min <- function(gs_data){
 } #need to code
 
@@ -108,36 +136,50 @@ by_none <- function(gs_data){
 #' @export
 choose_aggregation <- function(aggregation, gs_data, sub_category, sub_assigns, weights = c()){
     if (aggregation ==  "weighted_by_points"){
-        return ( by_points(gs_data, sub_category = sub_category, 
-                           sub_assigns = sub_assigns) )
+        gs_data <- by_points(gs_data, sub_category = sub_category, 
+                             sub_assigns = sub_assigns)
     } else if (aggregation == "equally_weighted"){
-        return ( by_percent(gs_data, sub_category = sub_category, 
-                            sub_assigns = sub_assigns) )
-    } #else if (aggregation == "by_weight"){
-    #     return ( by_weight(gs_data, sub_category = sub_category, 
-    #                        sub_assigns = sub_assigns, weights = weights) )
-    # } else if (aggregation == "by_max"){
-    #     return ( by_max(gs_data, sub_category = sub_category, 
-    #                         sub_assigns = sub_assigns) )
-    # } else if (aggregation == "by_min"){
-    #     return ( by_min(gs_data, sub_category = sub_category, 
-    #                         sub_assigns = sub_assigns) )
-    # }
+        gs_data <- by_percent(gs_data, sub_category = sub_category, 
+                              sub_assigns = sub_assigns)
+    } else if (aggregation == "by_weight"){
+        gs_data <- by_weight(gs_data, sub_category = sub_category,
+                             sub_assigns = sub_assigns, weights = weights)
+    }else if (aggregation == "max"){
+        gs_data <- by_max(gs_data, sub_category = sub_category,
+                          sub_assigns = sub_assigns)
+    } #else if (aggregation == "by_min"){
+    #     gs_data <- by_min(gs_data, sub_category = sub_category,
+    #                       sub_assigns = sub_assigns)
+    # } 
+    else {
+        gs_data <- by_none(gs_data)
+    }
     
     #default == none
-    return (by_none(gs_data))
-}
+    return (gs_data)
+} 
 
-calculate_grades_with_for_loop <- function(gs_data, policy){
+
+#' Calculate Grades with For Loop
+#' 
+#' This function calculates grades with the by-functions using a for loop
+#'
+#' @param gs_data processed and prepped Gradescope data
+#' @param policy flattened and processed policy file
+#'
+#' @return the gs_data with appended subcategory and overall grades
+#' @export
+calculate_grades_with_for_loop <- function(gs_data, flat_policy){
     final_grades <- gs_data
-    for (i in 1:length(policy$name)){
-        if (policy$aggregation[i] == "by_weight"){
-            final_grades <- choose_aggregation(policy$category[i], policy$aggregation[i], unlist(policy$assignments[i]), final_grades, weights = policy$weights)
+    for (policy in flat_policy){
+        if (policy$aggregation == "by_weight"){
+            final_grades <- choose_aggregation(sub_category = policy$category, aggregation = policy$aggregation, sub_assigns = unlist(policy$assignments), gs_data = final_grades, weights = policy$weights)
         }
         else {
-            final_grades <- choose_aggregation(policy$category[i], policy$aggregation[i], unlist(policy$assignments[i]), final_grades) 
+            final_grades <- choose_aggregation( aggregation = policy$aggregation, gs_data = final_grades, sub_category = policy$category, sub_assigns = unlist(policy$assignments))
         }
     }
+    return (final_grades)
 }
 
 
@@ -155,7 +197,7 @@ create_overall_category <- function(policy_nested){
     assignments <- map(policy_nested, ~.x$category) |> unlist()
     weights <- map(policy_nested, ~.x$weight) |> unlist()
     i <- length(policy_nested)
-    overall_grade_category <- list(category = "Overall Grade",
+    overall_grade_category <- list(category = "overall_grade",
                                    aggregation = "by_weight",
                                    weights = weights,
                                    assignments = assignments)
