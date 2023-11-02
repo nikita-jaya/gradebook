b43403b3136bdc631e1c0ed01769788b824b3142
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-11-01 22:01:13 -0700

    compute_lateness function

--- R/lateness-in-pivot.R
+++ R/lateness-in-pivot.R
@@ -117,3 +117,45 @@ lateness_scores_in_wide <- function(pivotted_late_scores){
     return (wide_late_scores)
 }
 
+#' Replace Raw Scores with Lateness Scores
+#' This function replaces original raw points with the scores after lateness 
+#' policies have been applied.
+#'
+#' @param wide_processed_data A processed data in wide format
+#' @param lateness_scores A dataframe with scores after lateness
+#'
+#' @return A data frame
+#'
+#' @importFrom dplyr select left_join
+#' @export
+replace_raw_with_lateness_scores <- function(wide_processed_data, lateness_scores){
+    late_assigns <- names(lateness_scores)[names(lateness_scores) != "sid"] #all assignments with late scores
+    
+    after_lateness <- wide_processed_data |>
+        select(-late_assigns) |>
+        left_join(raw_scores, by = "sid")
+    
+    return (after_lateness)
+}
+
+#' Compute Lateness
+#' This function computes scores after lateness policy has been applied.
+#'
+#' @param wide_processed_data A processed data in wide format
+#' @param flat_policy A flattened policy file
+#'
+#' @return A data frame
+#'
+#' @export
+compute_lateness <- function(wide_processed_data, flat_policy){
+    lateness_table <- flat_policy |> create_lateness_table()
+    
+   after_lateness <-  wide_processed_data |>
+        pivot_gs() |>
+        pivot_for_lateness(lateness_table = lateness_table) |>
+        calculate_lateness() |>
+        lateness_scores_in_wide() |>
+        replace_raw_with_lateness_scores(wide_processed_data = wide_processed_data)
+   
+   return (after_lateness)
+}
