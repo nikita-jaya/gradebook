3f48708eb6e4c795c38d9dccc41a44598a02ad75
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-11-12 12:15:39 -0800

    tests for process_id

--- tests/testthat/test-process-gs.R
+++ tests/testthat/test-process-gs.R
@@ -1,3 +1,92 @@
+test_that("merge data has accurate dimensions - duplicate sid", {
+  data <- data.frame(
+    `SID` = c(3032412514, 3032412314, 3032412516,
+            3032412517, 3032412518, 3032412519, 3032412520, 3032412521, 3032412521),
+    
+    `Names` = c("John Smith", "Jane Doe", "Robert Brown", "Emily Johnson",
+              "Michael Davis", "Linda Wilson", "James Taylor", "Patricia Anderson", "Patricia Anderson"),
+    `Email` = c("john.smith@berkeley.edu", "jane.doe@berkeley.edu", "robert.brown@berkeley.edu",
+              "emily.johnson@berkeley.edu", "michael.davis@berkeley.edu",
+              "linda.wilson@berkeley.edu", "james.taylor@berkeley.edu",
+              "patricia.anderson@berkeley.edu", "patricia.anderson@berkeley.edu"
+    )
+  )
+  processed_data <- process_id(data)
+  
+  expect_equal(nrow(processed_data), 8)
+  expect_equal(ncol(processed_data), 3)
+})
+
+test_that("no duplicate student id's in sid column", {
+  data <- data.frame(
+    `SID` = c(3032412514, 3032412519, 3032412516,
+            3032412517, 3032412518, 3032412519, 3032412520, 3032412521),
+    
+    `Names` = c("John Smith", "Jane Doe", "Robert Brown", "Emily Johnson",
+              "Michael Davis", "Linda Wilson", "James Taylor", "Patricia Anderson"),
+    `Email` = c("john.smith@berkeley.edu", "jane.doe@berkeley.edu", "robert.brown@berkeley.edu",
+              "emily.johnson@berkeley.edu", "michael.davis@berkeley.edu",
+              "linda.wilson@berkeley.edu", "james.taylor@berkeley.edu",
+              "patricia.anderson@berkeley.edu" )
+  )
+  processed_data <- process_id(data)
+  
+  unique_sids <- length(unique(processed_data$SID))
+  total_rows <- nrow(processed_data)
+  
+  expect_equal(unique_sids, total_rows)
+})
+
+
+test_that("merges max value", {
+  data <- tibble::tibble(
+    `SID` = c(3032412521, 3032412521),
+    
+    `Names` = c("Patricia Anderson", "Patricia Anderson"),
+    `Email` = c("patricia.anderson@berkeley.edu", "patricia.anderson@berkeley.edu"),
+    `Lab 1` = c(0.8, NA),
+    `Lab 2` = c(0.9, NA),
+    `Lab 3` = c(NA, 0.85),
+    `Project 1` = c(0.9, 0.5), #even if you have 2 values - keep higher
+    `Project 2` = c(0, 1)
+  )
+  processed_data <- process_id(data)
+  
+  #the new data should look like this:
+  new_data <- tibble::tibble(
+    `SID` = c(3032412521),
+    `Names` = c("Patricia Anderson"),
+    `Email` = c("patricia.anderson@berkeley.edu"),
+    `Lab 1` = c(0.8),
+    `Lab 2` = c(0.9),
+    `Lab 3` = c(0.85),
+    `Project 1` = c(0.9),
+    `Project 2` = c(1)
+  )
+  
+  expect_equal(processed_data, new_data)
+})
+
+
+test_that("perfect data - change nothing", {
+  data <- data.frame(
+    `SID` = c(3032412514, 3032412314, 3032412516,
+            3032412517, 3032412518, 3032412519, 3032412520, 3032412521, 3032232521),
+    
+    `Names` = c("John Smith", "Jane Doe", "Robert Brown", "Emily Johnson",
+              "Michael Davis", "Linda Wilson", "James Taylor", "Patricia Anderson", "Patricia Jane"),
+    `Email` = c("john.smith@berkeley.edu", "jane.doe@berkeley.edu", "robert.brown@berkeley.edu",
+              "emily.johnson@berkeley.edu", "michael.davis@berkeley.edu",
+              "linda.wilson@berkeley.edu", "james.taylor@berkeley.edu",
+              "patricia.anderson@berkeley.edu", "patricia.jane@berkeley.edu"
+    )
+  )
+  processed_data <- process_id(data)
+  
+  expect_equal(nrow(processed_data), 9)
+  expect_equal(ncol(processed_data), 3)
+})
+
 
 test_that("pivot gs - works correctly", {
   data <- tibble::tibble(
