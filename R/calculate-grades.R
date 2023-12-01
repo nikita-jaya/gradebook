ef9c9271b5681cd8dcfae38fcbccf35be816b697
Nikita Jayaprakash <nikita.jp@gmail.com>
2023-12-01 07:54:40 -0800

    apply_clobber function

--- R/calculate-grades.R
+++ R/calculate-grades.R
@@ -224,3 +224,35 @@ convert_to_min <- function(hms){
   
   # placeholder for apply_clobbers()
 }
+
+#' Apply Clobber Policy
+#'
+#' @description
+#' This computes any clobber policy, where the category grade is determined by the
+#' maximum score between two categories.
+#'
+#' @param gs A vector of assignment scores and weights coming from a row of the
+#' gs data frame.
+#' @param policy A single-layer list containing, at least `assignments`,
+#' a vector and optionally `n_drops`, an integer.
+#' 
+#' @return A gs dataframe with clobber policies applied to relevant categories
+#' @export
+apply_clobber <- function(gs, policy){
+  clobber_policy <- purrr::map(policy$categories, function(x){
+    return (c(x$category, x$clobber))
+  }) |>
+    purrr::discard(function(p){
+      (length(p) != 2)
+    }) 
+  
+  if (length(clobber_policy) == 0){
+    return (gs)
+  }
+  #apply clobber
+  for (clobber in clobber_policy){
+    gs[[clobber[1]]] <- pmax(gs[[clobber[1]]], gs[[clobber[2]]])
+  }
+  
+  return (gs)
+}
