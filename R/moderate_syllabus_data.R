5afe344d62fd95dc6bed66842c9f6cfaf7ba4712
Nikita Jayaprakash <nikita.jp@gmail.com>
2024-05-16 15:21:06 -0700

    moderate syllabus data

---
+++ R/moderate_syllabus_data.R
@@ -0,0 +1,11 @@
+#' Mmoderate Syllabus Data
+#'
+#' Sample grades data for a moderately complex syllabus, shown in "Case Study: A Moderate Syllabus" data
+#' 
+#'
+#' @examples
+#' moderate_syllabus_data
+#' 
+#' # yaml:as_yaml(moderate_syllabus_data)
+#' 
+"moderate_syllabus_data"
