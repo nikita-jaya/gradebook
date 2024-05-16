c0befde3c30e6341dda5b9835b65c2d7e8f950cf
Nikita Jayaprakash <nikita.jp@gmail.com>
2024-05-16 14:38:15 -0700

    simple syllabus data

---
+++ R/simple_syllabus_data.R
@@ -0,0 +1,11 @@
+#' Simple Syllabus Data
+#'
+#' Sample grades data for a simple syllabus, shown in "Case Study: A Simple Syllabus" data
+#' 
+#'
+#' @examples
+#' simple_syllabus_data
+#' 
+#' # yaml:as_yaml(simple_syllabus_data)
+#' 
+"simple_syllabus_data"
