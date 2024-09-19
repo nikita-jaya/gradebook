41091ca3b186c875cd7b60293493b5dc66cf9f52
nikita-jaya <nikita.jp@gmail.com>
2024-09-19 10:47:41 -0700

    bringing back drop_ungraded_assignments

--- R/process-gs.R
+++ R/process-gs.R
@@ -19,6 +19,28 @@ process_gs <- function(gs, drop_ungraded = TRUE, verbose = FALSE){
   gs
 }
 
-drop_ungraded_assignments <- function(gs, verbose = verbose){
-  gs #needs to be written
+#' @importFrom dplyr filter select
+#' @importFrom purrr keep
+#' @importFrom cli cli_alert_info cli_div cli_text cli_end
+drop_ungraded_assignments <- function(gs, verbose = FALSE) {
+  # This functions drops any assignments that have no grades for any students and replaced -Inf values
+  assignments <- get_assignments(gs)
+  #These are the dropped assignments with all NAs for raw-score
+  dropped <- gs |> purrr::keep(~all(is.na(.x))) |> names()
+  dropped <- dropped[dropped %in% assignments]
+  
+  alert <- function() {
+    cli::cli_div(theme = list(span.emph = list(color = "orange")))
+    cli::cli_text("{.emph Important Message}")
+    cli::cli_end()
+    if (length(dropped) != 0) {
+      cli::cli_alert_info("These are your ungraded assignments: {dropped}")
+    } else {
+      cli::cli_alert_info("These are no ungraded assignments")
+    }
+  }
+  if (verbose){
+    alert()
+  }
+  gs |> select(-contains(dropped))
 }
